a28e649f408e9d75309647c1571135fea68cbcc8
GLEESON <N3024253@live.tees.ac.uk>
2017-01-11 10:55:01 +0000

    Started adding unit test info

--- SelectionBoxService/Data/FakeDb.cs
+++ SelectionBoxService/Data/FakeDb.cs
@@ -8,22 +8,60 @@ using System.Threading.Tasks;
 
 namespace SelectionBoxService.Data
 {
-    public partial class FakeDb : DbInterface
+    public partial class FakeDb : DbContext, DbInterface
     {
+        
+        public FakeDb()
+        {
+            this.Products = Set<Product>();
+            this.SelectionBoxes = Set<SelectionBox>();
+            this.SelectionBoxProducts = Set<SelectionBoxProduct>();
+
+            Products.RemoveRange(Products);
+            SelectionBoxes.RemoveRange(SelectionBoxes);
+            SelectionBoxProducts.RemoveRange(SelectionBoxProducts);
+
+            SaveChanges();
+
+            for (int i = 1; i <= 20; i++)
+            {
+                Products.Add(new Product
+                {
+                    Name = "Product" + i,
+                    Store = "FakeStore",
+                    ProductId = i,
+                    Id = i
+                });
+            }
+
+            SaveChanges();
+
+            for (int i = 1; i < 10; i++)
+            {
+                SelectionBoxes.Add(new SelectionBox
+                {
+                    Id = i,
+                    Total = 10.0,
+                    Wrapping = "Paper",
+                    Removed = false,
+                    Visible = true,
+                    Available = true
+                });
+            }
+
+            SaveChanges();
+
+            for (int i = 0; i <= 20; i++)
+            {
+                
+            }
+        }
+
         public virtual DbSet<Product> Products { get; set; }
 
         public virtual DbSet<SelectionBox> SelectionBoxes { get; set; }
 
         public virtual DbSet<SelectionBoxProduct> SelectionBoxProducts { get; set; }
-
-        public System.Data.Entity.Infrastructure.DbEntityEntry Entry(object entity)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<int> SaveChangesAsync()
-        {
-            throw new NotImplementedException();
-        }
+        
     }
 }
