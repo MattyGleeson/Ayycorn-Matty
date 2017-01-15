ef6bdab4513916daeaff85b14da34c4c6b90da70
GLEESON <N3024253@live.tees.ac.uk>
2017-01-15 23:19:02 +0000

    More progress on web tests

--- SelectionBoxService.WebTests/WebTest1Coded.cs
+++ SelectionBoxService.WebTests/WebTest1Coded.cs
@@ -14,73 +14,41 @@ namespace SelectionBoxService.WebTests
     using System.Collections.Generic;
     using System.Text;
     using Microsoft.VisualStudio.TestTools.WebTesting;
-
+    using LibAyycorn.Dtos;
+    using Newtonsoft.Json;
+    using System.Linq;
 
     public class WebTest1Coded : WebTest
     {
+        private string json;
+        private List<Giftbox> Boxes;
+        private readonly JsonSerializerSettings SerializerSettings = new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore, MissingMemberHandling = MissingMemberHandling.Ignore };
+
 
         public WebTest1Coded()
         {
             this.PreAuthenticate = true;
             this.Proxy = "default";
+            json = System.IO.File.ReadAllText("../../../SelectionBoxService.WebTests/Data/selectionboxes.json");
+            Boxes = JsonConvert.DeserializeObject<List<Giftbox>>(json, SerializerSettings);
         }
 
         public override IEnumerator<WebTestRequest> GetRequestEnumerator()
         {
-            WebTestRequest request1 = new WebTestRequest("http://ayycornselectionboxservice.azurewebsites.net/postbox");
-            request1.Encoding = System.Text.Encoding.GetEncoding("utf-8");
+            WebTestRequest request1 = new WebTestRequest("http://ayycornselectionboxservice.azurewebsites.net/getboxes");
             yield return request1;
             request1 = null;
 
             WebTestRequest request2 = new WebTestRequest("http://ayycornselectionboxservice.azurewebsites.net/postbox");
+            request2.Method = "POST";
             request2.Encoding = System.Text.Encoding.GetEncoding("utf-8");
+            StringHttpBody request2Body = new StringHttpBody();
+            request2Body.ContentType = "application/json";
+            request2Body.InsertByteOrderMark = false;
+            request2Body.BodyString = JsonConvert.SerializeObject(Boxes.ElementAt(0));
+            request2.Body = request2Body;
             yield return request2;
             request2 = null;
-
-            WebTestRequest request3 = new WebTestRequest("http://ayycornselectionboxservice.azurewebsites.net/postbox");
-            request3.Encoding = System.Text.Encoding.GetEncoding("utf-8");
-            yield return request3;
-            request3 = null;
-
-            WebTestRequest request4 = new WebTestRequest("http://ayycornselectionboxservice.azurewebsites.net/postbox");
-            request4.Encoding = System.Text.Encoding.GetEncoding("utf-8");
-            yield return request4;
-            request4 = null;
-
-            WebTestRequest request5 = new WebTestRequest("http://ayycornselectionboxservice.azurewebsites.net/postbox");
-            request5.Encoding = System.Text.Encoding.GetEncoding("utf-8");
-            yield return request5;
-            request5 = null;
-
-            WebTestRequest request6 = new WebTestRequest("http://ayycornselectionboxservice.azurewebsites.net/postbox");
-            request6.Encoding = System.Text.Encoding.GetEncoding("utf-8");
-            yield return request6;
-            request6 = null;
-
-            WebTestRequest request7 = new WebTestRequest("http://ayycornselectionboxservice.azurewebsites.net/getboxes");
-            request7.Encoding = System.Text.Encoding.GetEncoding("utf-8");
-            yield return request7;
-            request7 = null;
-
-            WebTestRequest request8 = new WebTestRequest("http://ayycornselectionboxservice.azurewebsites.net/getboxes");
-            request8.Encoding = System.Text.Encoding.GetEncoding("utf-8");
-            yield return request8;
-            request8 = null;
-
-            WebTestRequest request9 = new WebTestRequest("http://ayycornselectionboxservice.azurewebsites.net/getboxes");
-            request9.Encoding = System.Text.Encoding.GetEncoding("utf-8");
-            yield return request9;
-            request9 = null;
-
-            WebTestRequest request10 = new WebTestRequest("http://ayycornselectionboxservice.azurewebsites.net/getboxes");
-            request10.Encoding = System.Text.Encoding.GetEncoding("utf-8");
-            yield return request10;
-            request10 = null;
-
-            WebTestRequest request11 = new WebTestRequest("http://ayycornselectionboxservice.azurewebsites.net/getboxes");
-            request11.Encoding = System.Text.Encoding.GetEncoding("utf-8");
-            yield return request11;
-            request11 = null;
         }
     }
 }
